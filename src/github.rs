5e3bf481c15c15c0486b9f195de722906f41c9d7
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2024-12-20 21:57:13 +0000

    extract owner and name from repository url

--- src/github.rs
+++ src/github.rs
@@ -1,5 +1,7 @@
 use anyhow::{Context, Error};
 
+const API_BASE_URL: &str = "https://api.github.com/repos/";
+
 pub struct GitHubClient {
     client: reqwest::blocking::Client,
 }
@@ -13,7 +15,12 @@ impl GitHubClient {
     }
 
     pub fn get_contributor_count(&self, repo_url: String) -> Result<u16, Error> {
-        let contributors_url = format!("{}/contributors", repo_url);
+        let owner_and_name = match repo_url.strip_prefix("https://github.com/") {
+            None => return Err(anyhow::anyhow!("could not extract owner and name from repository url")),
+            Some(owner_and_name) => owner_and_name,
+        };
+
+        let contributors_url = format!("{}/{}/contributors", API_BASE_URL, owner_and_name);
 
         match serde_json::from_str::<serde_json::Value>(&self.get(&contributors_url)?)
             .with_context(|| format!("failed to deserialize response from: {}", contributors_url))?
