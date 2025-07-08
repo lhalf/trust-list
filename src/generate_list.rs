84ddc0525ee0fd2d6b7e54de4cf573e9da0d3cde
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-07-08 22:11:31 +0100

    appends expected line to table

--- src/generate_list.rs
+++ src/generate_list.rs
@@ -117,7 +117,6 @@ mod tests {
             .get
             .returns
             .push_back(Ok(include_str!("../tests/data/crate_info.json").to_string()));
-
         http_client_spy
             .get
             .returns
@@ -126,6 +125,7 @@ mod tests {
         assert!(generate_list(crates, file_io_spy, http_client_spy).is_ok())
     }
 
+    // TODO: this should probably not fail
     #[test]
     fn single_crate_required_get_contributor_count_fails() {
         let crates = BTreeSet::from(["autospy".to_string()]);
@@ -141,11 +141,9 @@ mod tests {
             .get
             .returns
             .push_back(Ok(include_str!("../tests/data/crate_info.json").to_string()));
-
         http_client_spy.get.returns.push_back(Ok(
             r#"{ "dependencies": [], "versions": [], "meta": { "total": 32 } }"#.to_string(),
         ));
-
         http_client_spy
             .get
             .returns
@@ -156,6 +154,77 @@ mod tests {
             generate_list(crates, file_io_spy, http_client_spy)
                 .unwrap_err()
                 .to_string()
+        )
+    }
+
+    #[test]
+    fn single_crate_required_appending_table_fails() {
+        let crates = BTreeSet::from(["autospy".to_string()]);
+        let file_io_spy = FileIOSpy::default();
+        let http_client_spy = GetRequestSpy::default();
+
+        file_io_spy
+            .read_to_string
+            .returns
+            .push_back(Ok(String::new()));
+
+        http_client_spy
+            .get
+            .returns
+            .push_back(Ok(include_str!("../tests/data/crate_info.json").to_string()));
+        http_client_spy.get.returns.push_back(Ok(
+            r#"{ "dependencies": [], "versions": [], "meta": { "total": 32 } }"#.to_string(),
+        ));
+        http_client_spy
+            .get
+            .returns
+            .push_back(Ok(r#"[1,2,3,4,5]"#.to_string()));
+
+        file_io_spy
+            .append
+            .returns
+            .push_back(Err(anyhow::anyhow!("deliberate test error")));
+
+        assert_eq!(
+            "deliberate test error",
+            generate_list(crates, file_io_spy, http_client_spy)
+                .unwrap_err()
+                .to_string()
+        )
+    }
+
+    #[test]
+    fn single_crate_required_appends_expected_line_to_table() {
+        let crates = BTreeSet::from(["autospy".to_string()]);
+        let file_io_spy = FileIOSpy::default();
+        let http_client_spy = GetRequestSpy::default();
+
+        file_io_spy
+            .read_to_string
+            .returns
+            .push_back(Ok(String::new()));
+
+        http_client_spy
+            .get
+            .returns
+            .push_back(Ok(include_str!("../tests/data/crate_info.json").to_string()));
+        http_client_spy.get.returns.push_back(Ok(
+            r#"{ "dependencies": [], "versions": [], "meta": { "total": 32 } }"#.to_string(),
+        ));
+        http_client_spy
+            .get
+            .returns
+            .push_back(Ok(r#"[1,2,3,4,5]"#.to_string()));
+
+        file_io_spy.append.returns.push_back(Ok(()));
+
+        assert!(generate_list(crates, file_io_spy.clone(), http_client_spy).is_ok());
+        assert_eq!(
+            vec![
+                b"|autospy|1861|5|32|8|15/05/2025|01/07/2025|https://github.com/lhalf/autospy|\n"
+                    .to_vec()
+            ],
+            file_io_spy.append.arguments.take_all()
         )
     }
 }
